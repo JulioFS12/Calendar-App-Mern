3f058e45176583d0f01d082951c8aab9d1499920
Julio Flores <juliocesarflores12@gmail.com>
2020-11-27 10:43:09 -0400

    calendar app first commit

---
+++ MERN-Calendar-Front/src/helpers/calendar-messages-es.js
@@ -0,0 +1,15 @@
+export const messages = {
+    allDay: 'All day',
+    previous: '<',
+    next: '>',
+    today: 'Today',
+    month: 'Month',
+    week: 'Week',
+    day: 'Day',
+    agenda: 'Diary',
+    date: 'Date',
+    time: 'Hour',
+    event: 'Evnte',
+    noEventsInRange: 'There are not events',
+    showMore: total => `+ See more (${total})`
+};
